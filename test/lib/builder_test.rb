9d6bfddbc55a095f4914d58fb15830436e114c3f
Benjamin <ashanin.benjamin@gmail.com>
2013-11-02 16:48:58 +0400

    change test

--- test/lib/builder_test.rb
+++ test/lib/builder_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class BuilderTest < TestCase
   def setup
-    @builder = Configus::Builder.new do
+    @builder = Configus::Builder.build :test do
       env :test do
         c 'c'
         a do
@@ -12,11 +12,11 @@ class BuilderTest < TestCase
     end
   end
 
-  #def test_simple
-  #  assert_equal 'c', @builder.build[:c]
-  #end
-  #
-  #def test_nested
-  #  assert_equal 'b', @builder.build[:a][:b]
-  #end
+  def test_simple
+   assert_equal 'c', @builder[:c]
+  end
+
+  def test_nested
+   assert_equal 'b', @builder[:a][:b]
+  end
 end
